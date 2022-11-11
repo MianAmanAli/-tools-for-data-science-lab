4f8a893ec8c62f5211d1e3ab0ccb36ed5d28ffec
MianAmanAli <68514024+MianAmanAli@users.noreply.github.com>
2022-11-11 15:16:49 +0500

    Add files via upload

---
+++ lab task for tools in data science.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>My Jupyter Notebook on IBM Watson Studio<h1>
+
+# <b> AMAN ALI </b> 
+# <br>
+# Data scientist
+
+# <i> I am interested in Data Science because future lay ahead of Big data and data Science, it is data which yeald us tremendous results.</i>
+
+# <h3> the code below is just addition of two intigers <h3>
+
+# In[24]:
+
+
+x = 5
+y = 10
+print(x + y)
+
+
+# <h3>Using HTML In markdown, this cell must include</h3>
+# <b>it can be Bullets</b>
+# <ul> 
+# <li> bulleted 1</li>
+#     <li> bulleted 2</li>
+# </ul>
+# <ol type = "1">
+#     <li> Number One </li>
+#     <li> Number two </li>
+#     </ol>
+#     <table style = " left align"
+#            >
+#   <tr>
+#     <th>Company</th>
+#     <th>Contact</th>
+#     <th>Country</th>
+#   </tr>
+#     </table>
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
